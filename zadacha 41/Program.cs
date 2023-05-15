dea1aee7f668e17749d855eca1932a0f7aae84f3
kaktus <yuriykrivobrodskiy@yandex.ru>
2023-05-15 21:23:09 +0300

    zadacha41

---
+++ zadacha 41/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+﻿/* Пользователь вводит с клавиатуры M 
+чисел. Посчитайте, сколько чисел больше 0 ввёл 
+пользователь.
+*/ 
+
+void FullArray(int[] arr)
+{
+    for(int i=0; i<arr.Length; i++)
+    {
+        System.Console.Write($"Введите {i} число: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+string PrintArray(int[] arr)
+{
+    string res = "";
+    for(int i=0; i<(arr.Length)-1; i++)
+    {
+        res += Convert.ToString(arr[i]);
+        res += ", ";
+    }
+    res += Convert.ToString(arr[(arr.Length)-1]);
+    return res;
+}
+
+int CountArray(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]>0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+System.Console.Write("Введите количесто чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] my_array = new int[size];
+FullArray(my_array);
+System.Console.WriteLine($"{PrintArray(my_array)} -> {CountArray(my_array)}");
